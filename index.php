9d26dac140181fc2b4a81dc35897873193b0a2f6
Rohit Shukla <shukla.rohit.it@gmail.com>
2020-02-28 02:36:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
-<?php
-
-echo 'Hello Folks, this deployment through Jenkins for Docker Container';
-
-?>
+<html>
+  <title>Meetup world - DevOps Tools Demo!</title>
+  <body>
+    <?php
+      echo 'Hello Folks, this deployment through Jenkins for Docker Container';
+    ?>
+  </body>
+</html>
