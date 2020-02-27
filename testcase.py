e58a1e3a3008369a0e0099c1cdea0eb8d73142ba
Rohit Shukla <shukla.rohit.it@gmail.com>
2020-02-28 02:05:16 +0530

    Update testcase.py

--- testcase.py
+++ testcase.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 display = Display(visible=0, size=(1366, 768))
 display.start()
 browser = webdriver.Firefox()
-browser.get('http://13.235.27.172/')
+browser.get('http://13.235.27.172:8081/')
 print browser.title
 browser.quit()
 
