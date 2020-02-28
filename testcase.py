5e7fdb24917343db3653486fd853186978f23a9b
Rohit Shukla <shukla.rohit.it@gmail.com>
2020-02-28 14:31:07 +0530

    Update testcase.py

--- testcase.py
+++ testcase.py
@@ -4,7 +4,6 @@ from selenium import webdriver
 display = Display(visible=0, size=(1366, 768))
 display.start()
 browser = webdriver.Firefox()
-#browser.get('http://13.232.218.211:8081/')
 browser.get('https://google.com')
 print browser.title
 browser.quit()
